6dd8a4d689b3c15f2f41236db361c7086b85b38b
Jim <jimmontour@gmail.com>
2021-04-25 10:23:10 -0400

    update title

--- client/src/App.js
+++ client/src/App.js
@@ -20,9 +20,8 @@ const App = () => {
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography className={classes.heading} variant="h2" align="center">
-          Memories
+          TRAVELOG
         </Typography>
-        <img className={classes.image} src={memories} alt="icon" height="60" />
       </AppBar>
       <Grow in>
         <Container>
